f327004e7e073ef1f6e62c95a8f69ab7bf2dac0c
Okkio <60454980+ahmedhamad-okkio@users.noreply.github.com>
2021-08-11 10:46:38 +0100

    New Project WnS Added

--- src/Pages/ProjectsPage/ProjectsContainer.js
+++ src/Pages/ProjectsPage/ProjectsContainer.js
@@ -2,17 +2,26 @@ import React, {useEffect,useState} from 'react';
 import classes from './ProjectsContainer.module.css';
 import ProjectComponent from './ProjectComponent/ProjectComponent';
 import SudokuPreview from '../../Assets/Sudoku.png';
+import WnSPreview from '../../Assets/WokeAndShootThumb.png';
 
 
 
 const ProjectsContainer = (props) => {
     const [loadStyle,setLoadStyle] = useState({opacity:0,transform: `translateY(${50}px)`});
 
-    var desc1 = `This is an online Sudoku puzzle game with an intuitive UI,
+    var desc1 = [`This is an online Sudoku puzzle game with an intuitive UI,
      authentication and a leader board built using the MERN stack. It is a 
      responsive single page application fully designed and developed by me 
      from the ground up. A second version of this site was made using Firebase.`
-    var link = "https://sudoku-mern.herokuapp.com/#/"
+     ,<br/>,<i style={{fontSize:`${12}px`} }>~ Site may take up to 20 seconds to load</i>]
+    var link1 = "https://sudoku-mern.herokuapp.com/#/"
+
+    var desc2 = `WnS is an online multiplayer fast paced FPS game built
+     with Unreal Engine 4 primarily in C++ with the assistance of blueprints.
+     This is my first project on Unreal Engine and with C++, therefore I used this as a large scale
+     sandbox experience to familiarize myself with the engine's code base and to understand
+     new and challenging concepts such as networking and custom movement.`
+    var link2 = "https://github.com/ahmedhamad-okkio/WokeAndShoot"
 
 
 
@@ -39,7 +48,9 @@ const ProjectsContainer = (props) => {
         <>
         <div className={classes.Header}  id="ProjectsHeader" style={loadStyle}>FEATURED PROJECTS</div>
         <div className={classes.ProjectsContainer} style={loadStyle}>
-            <ProjectComponent projectName="SUDOKU MERN" desc={desc1} preview={SudokuPreview} link={link}/>
+            <ProjectComponent projectName="WnS (WIP)" desc={desc2} preview={WnSPreview} link={link2}/>
+            <ProjectComponent projectName="SUDOKU MERN" desc={desc1} preview={SudokuPreview} link={link1}/>
+
             {/* <ProjectComponent projectName="SUDOKU MERN" desc={desc1} preview={SudokuPreview} link={link}/> */}
             {/* <ProjectComponent projectName="Test" desc={"test2"} preview={"test"}/>  */}
         </div>
