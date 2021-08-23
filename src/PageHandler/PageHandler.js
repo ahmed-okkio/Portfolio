7e9bc06c8ddbf476c77bdeb3a3a312a8f9caa70c
Okkio <60454980+ahmedhamad-okkio@users.noreply.github.com>
2021-08-24 00:11:45 +0100

    Disabled site on small screen devices

--- src/PageHandler/PageHandler.js
+++ src/PageHandler/PageHandler.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import MainPage from '../Pages/MainPage/MainPage';
+import classes from '../Pages/IntroPage/IntroPage.module.css';
 import NavBar from '../Components/NavBar/NavBar';
 
 const PageHandler = () =>
 {
-
+    var width = window.innerWidth;
     return(
         <>
-            <NavBar></NavBar>
-            <MainPage/>
+            {width < 1300 
+            ?<div className={classes.mainBody}>    
+                <h1 className={classes.UnavialableText} >Sorry!</h1>
+                <p className={classes.UnavialableText} style={{fontSize:`${5}vw`}}>My portfolio is not currently compatible with your screen size</p>
+            </div>
+            :   <>
+                    <NavBar/> 
+                    <MainPage/>
+                </>
+            }
         </>
     )
-
-
-
-
 }
 export default PageHandler;
